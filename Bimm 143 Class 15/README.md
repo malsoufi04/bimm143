f3a72d2ae350ab2a0bb516ab1b6ecfd6a3b7e1e3
May Alsoufi <malsoufi@ucsd.edu>
2026-03-13 12:47:28 -0700

    upload BIMM143 labs

---
+++ Bimm 143 Class 15/README.md
@@ -0,0 +1,3 @@
+# bimm143
+
+This repo is for my UCSD bioinformatics class.
---
+++ Bimm/Lab4.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4 Intro"
+#' author: "May Alsoufi"
+#' date: "January 16th, 2026"
+#' ---
+x<-1:50
+plot(x)
+plot(x,sin(x))
+plot(x, sin(x), typ='l',col="blue", lwd=3,
+     xlab="Is it lunch time yet?")
